65c2516f895d201f179896969d2704108a840753
evananyonga <evewish@gmail.com>
2023-04-12 07:54:53 +0300

    Add documentation to APIs

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,14 +4,52 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 
 /**
  *  
- * APIs to manage user authentication apis 
-*/
+ * APIs to manage user authentication
+ *
+ * To access the API, a registered user must generate a token that they will pass to the headers of the Loan API
+ * 
+ * @bodyParam email string required The email used for the system. Example: email@email.com
+ * @bodyParam password string required Preferred password. Example: Pass&LSD2
+ * @bodyParam token_name string required The name of the system querying the Loan API. Example: BankSystem
+ * 
+ * If you are able to Login everything is okay, you'll get a 200 OK response with the response:.
+ * @respponse 200 scenario="Ok" 
+ * {
+    "token": "1|A9YZIwHj0ZP8GnhMN4a5HfDbCEoS6uFVRPUTwWhQ",
+    "user": {
+        "id": 1,
+        "name": "Some Admin",
+        "email": "someadmin@dfcu.co.ug",
+        "email_verified_at": null,
+        "created_at": "2023-10-19T12:57:16.000000Z",
+        "updated_at": "2021-10-19T12:57:16.000000Z"
+        }
+ *  }
+ * 
+ * Otherwise, the request will fail with a 422 error, and a response unprocessable content
+ * 
+ * @response 422 scenario="Unprocessable Content" {
+    "message": "The email field is required. (and 2 more errors)",
+    "errors": {
+        "email": [
+            "The email field is required."
+        ],
+        "password": [
+            "The password field is required."
+        ],
+        "token_name": [
+            "The token name field is required."
+        ]
+      }
+ * }
+ *
+ */
+
 class AuthController extends Controller
 {
    public function login(Request $request){
@@ -35,24 +73,5 @@ class AuthController extends Controller
             'message'=>'Username/Password Incorrect!',
             'status_code' => 403
         ], 403);
-    }
-
-   /**
-    * Logout a user
-    *
-    *
-    */
-   public function logOut(Request $request) {
-       $request->user()->tokens()->delete();
-
-        return response([
-            'message' => 'Logged out Successfully'
-        ], 200);
-    }
-
-    public function users()
-    {
-        $users = User::with(['role'])->latest()->paginate(10);
-        return view('auth.registered-users', compact('users'));
     }
 }
--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -9,8 +9,46 @@ use Illuminate\Http\Request;
 class LoanController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
+ *  
+ * API to view loan status
+ *
+ * To access the API, a registered user must pass a valid token to the headers of the Loan API
+ * 
+ * Accept: application/json
+ * Authorization: Bearer {token}
+ * 
+ * Without the valid headers, the user will get a response:
+ * @response 401 scenario="Unauthenticated" {"Unauthenticated"}
+ * 
+ * Without the right parameters, the response will be:
+ * @response 422 scenario="Unprocessable Content"
+ * 
+ * {
+    "message": "The account number field is required.",
+    "errors": {
+        "account_number": [
+            "The account number field is required."
+            ]
+        }
+ *  }
+ * 
+ * @bodyParam account_number required The Account Number in the system. Example: 5637294326
+ * 
+ * If you pass a ten-digit account number and everything is okay, you'll get a 200 OK response with the response:.
+ * @respponse 200 scenario="Ok" 
+ * {
+    "token": "1|A9YZIwHj0ZP8GnhMN4a5HfDbCEoS6uFVRPUTwWhQ",
+    "user": {
+        "id": 1,
+        "loan_amount": 500000,
+        "outstanding_balance": 20000,
+        "customer_id": 1,
+        "created_at": "2023-10-19T12:57:16.000000Z",
+        "updated_at": "2021-10-19T12:57:16.000000Z"
+        }
+ *  }
+ * 
+ */
 
     public function getStatus(Request $request){
 
